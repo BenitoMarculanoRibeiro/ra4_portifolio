87a5cea07c434ef6a14c2c5cb304a758f74b3b77
BenitoMarculanoRibeiro <49006573+BenitoMarculanoRibeiro@users.noreply.github.com>
2025-02-14 13:04:16 -0300

    Primeiro Commit Projeto iniciado com informações vagas com a finalidade apenas de testar o servirdor, não levar esse commit a serio.

---
+++ app.py
@@ -0,0 +1,27 @@
+import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Carregar os dados do JSON
+def carregar_dados():
+    with open("data.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
+@app.route('/')
+def home():
+    dados = carregar_dados()
+    return render_template('index.html', sobre=dados["sobre"])
+
+@app.route('/portfolio')
+def portfolio():
+    dados = carregar_dados()
+    return render_template('portfolio.html', projetos=dados["projetos"])
+
+@app.route('/contato')
+def contato():
+    dados = carregar_dados()
+    return render_template('contato.html', sobre=dados["sobre"])
+
+if __name__ == '__main__':
+    app.run(debug=True)
