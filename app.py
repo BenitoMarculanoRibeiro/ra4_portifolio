49696086bb518f67bbacfb2a01859ae9604ca8da
BenitoMarculanoRibeiro <49006573+BenitoMarculanoRibeiro@users.noreply.github.com>
2025-02-18 21:56:00 -0300

    1

--- app.py
+++ app.py
@@ -23,5 +23,12 @@ def contato():
     dados = carregar_dados()
     return render_template('contato.html', sobre=dados["sobre"])
 
+@app.route('/c')
+def c():
+    return "c"
+@app.route('/d')
+def d():
+    return "d"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
